8fd22e12e5a4a6997966c5bab2a0f166ce8764cb
ruucm <ruucm.a@gmail.com>
2018-05-15 08:30:19 +0900

    refactor: About
    complete

---
+++ src/components/About/block/About/Title.js
@@ -0,0 +1,9 @@
+import styled, { css } from 'styled-components';
+
+
+const Title = styled.h1`
+  font-size: 24pt;
+  font-weight: 900;
+`
+
+export default Title;
---
+++ src/components/About/block/About/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import Title from './Title';
+import Desc from './Desc';
+
+const About = styled.div`
+  padding: 0 20px;
+`;
+
+About.Title = Title;
+About.Desc = Desc;
+
+export default About;
