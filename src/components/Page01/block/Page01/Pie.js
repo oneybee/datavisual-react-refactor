798804eebdcb294d5f1b1112cf19a05483b9a873
ruucm <ruucm.a@gmail.com>
2018-05-15 07:23:50 +0900

    refactor: InfoBannerComponent
    update pie

--- src/components/Page01/block/Page01/Pie.js
+++ src/components/Page01/block/Page01/Pie.js
@@ -1,7 +1,13 @@
+import React from 'react';
 import styled, { css } from 'styled-components';
+import { Column } from '../../../../libs/ruucm-blocks';
 
+const Pie = ({className, children}) => {
+  return (
+    <Column mCol='3' className={className}>
+      {children}
+    </Column>
+  )
+}
 
-const Title = styled.h1`
-`
-
-export default Title;
+export default Pie;
